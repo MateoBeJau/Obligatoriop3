0b766a0638f04e384502492b5c4f3cd84b73de8b
Mateo <mateobejau@gmail.com>
2023-06-30 18:46:35 -0300

    Creando Proyecto

---
+++ SistemaStock.Modelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaStock.Modelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
